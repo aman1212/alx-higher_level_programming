df7c30a2d2c4b6907f93f0d2eabc75e8a265acb4
aman <amanthebest132@gmail>
2022-06-04 15:03:06 +0300

    commited

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,19 @@
 #include "lists.h"
 /**
- * check_cycle - checks for a loop in a linked list
- * @list - pointer to a linked list
- *
- * Return: 1 if loop exists, 0 if not
+ *check_cycle - checks whether a singly linked list has a cycle in it
+ *@list:linked list to test
+ *Return:0 - no cycle, 1 - cycle exists
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *fast = list;
-	listint_t *slow = list;
+	listint_t *rabbit = list, *turtle = list;
 
-	if (list == NULL)
-		return (0);
-	if (list->next == NULL)
-		return (0);
-
-	while (fast != NULL && slow != NULL && fast->next != NULL)
+	while (rabbit && turtle && rabbit->next)
 	{
-		fast = fast->next->next;
-		slow = slow->next;
-
-		if (slow == fast)
+		rabbit = rabbit->next->next;
+		turtle = turtle->next;
+		if (rabbit == turtle)
 			return (1);
 	}
 	return (0);
-}
+} 
--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -1,1 +1,1 @@
-Hello world directory 
+**introductory to python**
