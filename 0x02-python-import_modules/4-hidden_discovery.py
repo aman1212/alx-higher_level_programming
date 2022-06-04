cd4853cacb0f0811b54a9b7a9ef08f71e289f9c0
aman <amanthebest132@gmail>
2022-06-04 15:10:09 +0300

    commited

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    list = dir(hidden_4)
-    for j in list:
-        if j[0] != '_':
-            print("{}".format(j))
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
