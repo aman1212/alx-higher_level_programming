8d174863ca241256c58aa0cbda4a8c8af247feeb
aman <amanthebest132@gmail>
2022-06-06 08:26:06 +0300

    commited

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 
 
 def element_at(my_list, idx):
-    if(len(my_list)<= idx):
+    if len(my_list)<= idx :
         return None
-    elif(idx < 0):
+    elif idx < 0 :
         return None
     else:
         return my_list[idx]
