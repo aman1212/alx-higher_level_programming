c1d22faee5fa82ce33ed67e5fdd5c7b67819ef06
aman <amanthebest132@gmail>
2022-06-06 08:31:00 +0300

    commited

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,10 @@
 
 
 def element_at(my_list, idx):
+    """Retrive an element from a list."""
     if len(my_list)<= idx :
         return None
     elif idx < 0 :
         return None
-    else:
+    else :
         return my_list[idx]
