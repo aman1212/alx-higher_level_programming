5c7c78e11c00c697cf99e0adeac3cecdc5f62d0b
aman <amanthebest132@gmail>
2022-07-05 13:49:02 +0300

    commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-Definition of module
-"""
+"""write file"""
 
 def write_file(filename="", text=""):
-    """writes a string to a text file and returns the number of characters written
-    Args
-       filename - file name
-       text - string to be written
-    """
+    """"write file"""
+    
     with open(filename, "w", encoding="utf-8") as f:
         data = f.write(text)
         return data
